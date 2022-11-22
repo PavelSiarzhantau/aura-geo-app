4e88a29ad2e76b576c1003ad69c983a392297bb9
Pavel Siarzhantau <pavel.siarzhantau@gmail.com>
2022-11-22 19:50:30 +0100

    feat: add code to aura-geo-app project

---
+++ force-app/main/default/aura/AccountMap/AccountMapController.js
@@ -0,0 +1,29 @@
+// Code Highlights
+
+// The onAccountsLoaded function handles the c:AccountsLoaded event (as specified in the component markup) and updates the mapMarkers attribute bound to the <lightning:map> tag.
+// event.getParam( 'accounts' ) gets the account records from the event. Remember that the c:AccountsLoaded event markup declares this attribute and the c:AccountSearch component sets the value when it fires the event.
+({
+    onAccountsLoaded: function( component, event, helper ) {
+        var mapMarkers = [];
+        var accounts = event.getParam( 'accounts' );
+        for ( var i = 0; i < accounts.length; i++ ) {
+            var account = accounts[i];
+            var marker = {
+                'location': {
+                    'Street': account.BillingStreet,
+                    'City': account.BillingCity,
+                    'PostalCode': account.BillingPostalCode
+                },
+                'title': account.Name,
+                'description': (
+                    'Phone: ' + account.Phone +
+                    ' ' +
+                    'Website: ' + account.Website
+                ),
+                'icon': 'standard:location'
+            };
+            mapMarkers.push( marker );
+        }
+        component.set( 'v.mapMarkers', mapMarkers );
+    }
+})
---
+++ force-app/main/default/aura/AccountSearch/AccountSearchController.js
@@ -0,0 +1,30 @@
+// Code Highlights
+
+// component.get( "v.searchTerm" ) gets the user’s search term—the text value bound to the <lightning:input> tag.
+// The helper.handleSearch(component, searchTerm) function encapsulates the call to the server-side controller to 
+// perform the search.Helper functions promote JavaScript code reuse within a component bundle.
+({
+    onInit: function( component, event, helper ) {
+        // proactively search on component initialization
+        var searchTerm = component.get( "v.searchTerm" );
+        helper.handleSearch( component, searchTerm );
+    },
+    onSearchTermChange: function( component, event, helper ) {
+        // search anytime the term changes
+        var searchTerm = component.get( "v.searchTerm" );
+        // to improve performance, particularly for fast typers,
+        // we wait a small delay to check when user is done typing
+        var delayMillis = 500;
+        // get timeout id of pending search action
+        var timeoutId = component.get( "v.searchTimeoutId" );
+        // cancel pending search action and reset timer
+        clearTimeout( timeoutId );
+        // delay doing search until user stops typing
+        // this improves client-side and server-side performance
+        timeoutId = setTimeout( $A.getCallback( function() {
+            helper.handleSearch( component, searchTerm );
+        }), delayMillis);
+        //JS MDN investigate
+        component.set( "v.searchTimeoutId", timeoutId );
+    }
+})
